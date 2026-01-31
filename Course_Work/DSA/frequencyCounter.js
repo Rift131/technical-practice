81ab619be161e7da2a6e56fa15400a7d69bf6d6a
Ken Pederson <102421691+Rift131@users.noreply.github.com>
2026-01-31 15:21:50 -0600

    Converted tests to use assertEquals pattern

--- Course_Work/DSA/frequencyCounter.js
+++ Course_Work/DSA/frequencyCounter.js
@@ -28,7 +28,7 @@ function isAnagram(str1, str2) {
   for (const char of str1Clean) {
     const validChar = isChar(char);
     if (validChar) {
-      str1Chars[char] ? str1Chars[char]++ : (str1Chars[char] = 1);
+      str1Chars[char] ? (str1Chars[char] += 1) : (str1Chars[char] = 1);
     } else if (!validChar) {
       return false;
     }
@@ -36,24 +36,53 @@ function isAnagram(str1, str2) {
   // 2. With the object populated by the first string, if we subtact each occurrence of a letter fromt he second string, the value for each letter should become 0 when we're done iterating. If not, then the two objects do not formulate an anagram
   for (const char of str2Clean) {
     const validChar = isChar(char);
-    if (validChar && str1Chars[char]) {
+    if (validChar && str1Chars[char] && str1Chars[char] > 0) {
       str1Chars[char] -= 1;
     } else {
       return false;
     }
   }
+
   return true;
 }
 
 /**
  * Tests
  **/
-console.log(isAnagram("text9twisttime", "3time")); // false
-console.log(isAnagram("", "")); // true
-console.log(isAnagram("aaZ", "zza")); // false
-console.log(isAnagram("anaGram", "nagaram")); // true
-console.log(isAnagram("rat", "caR")); // false
-console.log(isAnagram("awesome", "awesom")); // false
-console.log(isAnagram("qwerty", "qeywrt")); // true
-console.log(isAnagram("texttwisttime", "timetwisttext")); // true
-console.log(isAnagram("texttwisTTime", "timetwisttext")); // true
+
+const assertIsAnagram = (actual, expected, testName) => {
+  if (actual === expected) {
+    console.log("Success");
+  } else {
+    console.log(
+      `Expected ${expected}, however the result was ${actual} for ${testName}`,
+    );
+  }
+};
+
+/** True Tests */
+const emptyText = isAnagram("", "");
+assertIsAnagram(emptyText, true, "emptyText");
+
+const upperCaseSome = isAnagram("anaGram", "nagaram");
+assertIsAnagram(upperCaseSome, true, "upperCaseSome");
+
+const longTestAnagram = isAnagram("texttwisttime", "timetwisttext");
+assertIsAnagram(longTestAnagram, true, "longTestAnagram");
+
+/** False Tests */
+
+const upperCaseSomeNonAnagram = isAnagram("aaZ", "zza");
+assertIsAnagram(upperCaseSomeNonAnagram, false, "upperCaseSomeNonAnagram");
+
+const hasNoCharButIsEqual = isAnagram("%^&*(^65576587", "%^&*(^56755678");
+assertIsAnagram(hasNoCharButIsEqual, false, "hasNoCharButIsEqual");
+
+const hasNoCharNotEqual = isAnagram("6856-+-", "65--");
+assertIsAnagram(hasNoCharNotEqual, false, "hasNoCharNotEqual");
+
+const numsInText = isAnagram("text9twisttime", "3time");
+assertIsAnagram(numsInText, false, "numsInText");
+
+const hasMissingLetter = isAnagram("awesome", "awesom");
+assertIsAnagram(hasMissingLetter, false, "hasMissingLetter");
