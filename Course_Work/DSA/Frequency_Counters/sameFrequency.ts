727d7ce31f00b8fae3402f1518f464908241b8c3
Ken Pederson <102421691+Rift131@users.noreply.github.com>
2026-02-13 08:21:17 -0600

    gitignore added, ignoring node_modules

--- Course_Work/DSA/Frequency_Counters/sameFrequency.ts
+++ Course_Work/DSA/Frequency_Counters/sameFrequency.ts
@@ -1,5 +1,21 @@
 const sameFrequency = (num1: number, num2: number): boolean => {
-  return false;
+  /**Short-circuit */
+  if (String(num1).length !== String(num2).length) return false;
+
+  let freq: Record<string, number> = {};
+  /** Build up the count of each numbers appearance.
+   * To make each number within the number itself
+   * iterable, it must be converted to a string */
+  for (const num of String(Math.abs(num1))) {
+    /** Math.abs ensured the "-" isn't counted as a character for negative nums */
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  for (const num of String(Math.abs(num2))) {
+    if (!freq[num] || freq[num] === 0) return false;
+    freq[num] = freq[num] -= 1;
+  }
+  return true;
 };
 
 const assertEquals = (
@@ -7,7 +23,15 @@ const assertEquals = (
   expected: boolean,
   testName: string,
 ): boolean => {
-  return false;
+  if (actual === expected) {
+    console.log(`${testName} passed!`);
+    return true;
+  } else {
+    console.log(
+      `${testName} was expected to result in ${expected} but resulted in ${actual}.`,
+    );
+    return false;
+  }
 };
 
 /** Command to run: node Course_Work/DSA/Frequency_Counters/sameFrequency.ts */
@@ -18,4 +42,4 @@ console.log(assertEquals(isTensFalse, false, "isTensFalse"));
 const isMillionsTrue = sameFrequency(3589578, 5879385);
 console.log(assertEquals(isMillionsTrue, true, "isMillionsTrue"));
 const isTenAndHundredFalse = sameFrequency(22, 222);
-console.log(assertEquals(isTenAndHundredFalse, true, "isTenAndHundredFalse"));
+console.log(assertEquals(isTenAndHundredFalse, false, "isTenAndHundredFalse"));
